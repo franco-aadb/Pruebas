0af6f1d6b84509e9e39b95618f84b6d73cf4e6a5
franco-aadb <franco.duran@inrpac.cl>
2023-09-05 11:11:16 -0300

    cambio variable CategoryListAV-post

--- internalManagementProject/projectlist_app/api/views.py
+++ internalManagementProject/projectlist_app/api/views.py
@@ -12,12 +12,12 @@ class CategoryListAV(APIView):
         return Response(serializer.data)
 
     def post(self,request):
-        serializer = CategorySerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+        serializer2 = CategorySerializer(data = request.data)
+        if serializer2.is_valid():
+            serializer2.save()
+            return Response(serializer2.data)
         else:
-            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+            return Response(serializer2.errors, status = status.HTTP_400_BAD_REQUEST)
 
 class CategoryDetailAV(APIView):
     def get(self,request,key):
